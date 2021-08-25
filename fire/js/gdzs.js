6857bb9063dfa4921137f8f261aab56b233d9247
aleksei18rus <aleksei18rus@gmail.com>
2021-08-25 20:03:19 +0400

    a
    gdzs

--- fire/js/gdzs.js
+++ fire/js/gdzs.js
@@ -1,46 +1,87 @@
 function gdzs() {
     document.querySelector('.b-1').addEventListener('click', ()=>{
         var p_min_on = document.querySelector('.p_min_on').value;
+        document.getElementById('p_min_on').textContent = p_min_on;
         var kof_szh;
             if (p_min_on >=200) {
                 kof_szh = 1.1;
             } else {
                 kof_szh = 1;
             }
-                if (document.querySelector('.usloviya').checked) {
-                    if (document.querySelector('.ochag').checked) {
+//-------------------------------------------------------------------------------------------------------------------------------- 
+                if (document.querySelector('.usloviya').checked) {  //проверка условий, сложные
+                    if (document.querySelector('.ochag').checked) { //очаг не найден
+                        var p_min_on = document.querySelector('.p_min_on').value;
+                        document.getElementById('p_min_on').textContent = p_min_on;
+                        var t_total_rab = ((p_min_on-10)*6.8)/(kof_szh* 40); //расчетобщее время работы
+                        document.getElementById('temp').textContent = Math.floor(t_total_rab); // общее время работы
                         var p_max_pad = (p_min_on-10)/3;
-                        document.getElementById('p_max_pad').textContent = 'Pmax.пад. '+p_max_pad+' атм.';
+                        document.getElementById('p_max_pad').textContent = Math.ceil(p_max_pad); //Р максимальное падения
                         var delta_t = (p_max_pad*6.8)/(kof_szh* 40);
-                        document.getElementById('delta_t').textContent = '∆T '+ delta_t.toFixed(1)+' мин.';
+                        document.getElementById('delta_t').textContent = Math.floor(delta_t); // Дельта Т
                     }
-                    else {
-                        var p_min_och = document.querySelector('.p_min_och').value;
-
+//--------------------------------------------------------------------------------------------------------------------------------                    
+                    else {  //очаг найден    
+                        var p_min_on = document.querySelector('.p_min_on').value;
+                        document.getElementById('p_min_on').textContent = p_min_on;
                         var t_total_rab = ((p_min_on-10)*6.8)/(kof_szh* 40);
-                        var p_max_pad = p_min_on - p_min_och;
-                        var p_kon_out = p_max_pad + (p_max_pad)+10;
-                        var t_rab_och = ((p_min_och - p_kon_out)*6.8)/(kof_szh* 40);
-                        document.getElementById('temp').textContent = t_total_rab.toFixed(1)+' мин.';
-                        document.getElementById('rab').textContent = t_rab_och.toFixed(1)+' мин.';
+                        document.getElementById('temp').textContent = Math.floor(t_total_rab); // общее время работы
+                        var p_min_och = document.querySelector('.p_min_och').value;
+                        if (p_min_och>0) {
+                            var p_max_pad = p_min_on - p_min_och;
+                            document.getElementById('p_zatrata').textContent = Math.ceil(p_max_pad); // Р затрачено
+                            var p_kon_out = p_max_pad + p_max_pad + 10;
+                            var t_rab_och = ((p_min_och - p_kon_out)*6.8)/(kof_szh* 40);
+                        } else {
+                            document.getElementById('p_zatrata').textContent = "Какое Pмин.очага?"; 
+                        }
+                        if (t_rab_och>0) {
+                            document.getElementById('rab').textContent = Math.floor(t_rab_och); // время работы у очага
+                        }
                     }
-                } else {
-                    if (document.querySelector('.ochag').checked) {
+//--------------------------------------------------------------------------------------------------------------------------------                    
+                } else {    // легкие условия
+                    if (document.querySelector('.ochag').checked) { //очаг не найден
+                        var p_min_on = document.querySelector('.p_min_on').value;
+                        document.getElementById('p_min_on').textContent = p_min_on;
+                        var t_total_rab = ((p_min_on-10)*6.8)/(kof_szh* 40); //расчетобщее время работы
+                        document.getElementById('temp').textContent = Math.floor(t_total_rab); // общее время работы
                         var p_max_pad = (p_min_on-10)/2.5;
-                        document.getElementById('p_max_pad').textContent = 'Pmax.пад. '+p_max_pad+' атм.';
+                        document.getElementById('p_max_pad').textContent = Math.ceil(p_max_pad); //Р максимальное падения
                         var delta_t = (p_max_pad*6.8)/(kof_szh* 40);
-                        document.getElementById('delta_t').textContent = '∆T '+ delta_t.toFixed(1)+' мин.';
+                        document.getElementById('delta_t').textContent = Math.floor(delta_t); // Дельта Т
                     }
-                    else {
-                        var p_min_och = document.querySelector('.p_min_och').value;
-
+//-------------------------------------------------------------------------------------------------------------------------------- 
+                    else { // очаг найден
+                        var p_min_on = document.querySelector('.p_min_on').value;
+                        document.getElementById('p_min_on').textContent = p_min_on;
                         var t_total_rab = ((p_min_on-10)*6.8)/(kof_szh* 40);
-                        var p_max_pad = p_min_on - p_min_och;
-                        var p_kon_out = p_max_pad + (p_max_pad/2)+10;
-                        var t_rab_och = ((p_min_och - p_kon_out)*6.8)/(kof_szh* 40);
-                        document.getElementById('temp').textContent = t_total_rab.toFixed(1)+' мин.';
-                        document.getElementById('rab').textContent = t_rab_och.toFixed(1)+' мин.';
+                        document.getElementById('temp').textContent = Math.floor(t_total_rab); // общее время работы
+                        var p_min_och = document.querySelector('.p_min_och').value;
+                        if (p_min_och>0) {
+                            var p_max_pad = p_min_on - p_min_och;
+                            document.getElementById('p_zatrata').textContent = Math.ceil(p_max_pad); // Р затрачено
+                            var p_kon_out = p_max_pad + (p_max_pad/2)+10;
+                            var t_rab_och = ((p_min_och - p_kon_out)*6.8)/(kof_szh* 40);
+                        } else {
+                            document.getElementById('p_zatrata').textContent = "Какое Pмин.очага?"; 
+                        }
+                        if (t_rab_och>0) {
+                            document.getElementById('rab').textContent = Math.floor(t_rab_och); // время работы у очага
+                        }    
                     }
                 }    
+    })
+
+    document.querySelector('.b-2').addEventListener('click', ()=>{
+        var clear;
+        document.getElementById('temp').textContent = clear;
+        document.getElementById('p_zatrata').textContent = clear;
+        document.getElementById('rab').textContent = clear;
+        document.getElementById('p_max_pad').textContent = clear;
+        document.getElementById('delta_t').textContent = clear;
+        document.getElementById('p_min_och').textContent = clear;
+        document.getElementById('p_min_on').textContent = clear;
+        document.getElementById('p_kon_out').textContent = clear;
     })
 }
